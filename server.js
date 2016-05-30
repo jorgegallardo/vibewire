2c126992b4396bee3750a795523df4c46596b7cb
Dave <dave.idell@gmail.com>
2016-05-30 13:24:56 -0400

    gitignore node_modules + move front end libraries to public folder

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 MongoClient.connect(url, function(err, db) {
   //ensure that we've connected
